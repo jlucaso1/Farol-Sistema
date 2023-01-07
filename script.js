c00efb717bdf460a2afb90531637aebcda6a4537
Gustavo lopes <gustavoclash3@gmail.com>
2023-01-07 17:08:19 -0300

    finalizacao-farol-base

--- script.js
+++ script.js
@@ -8,4 +8,18 @@ var btnVerde = document.getElementById("btnVerde")
 
 btnVermelho.addEventListener("click", function ligarFarol() {
     farolVermelho.classList.toggle("cor-vermelha")
-}) 
+    farolAmarelo.classList.remove("cor-amarela")
+    farolVerde.classList.remove("cor-verde")
+})
+
+btnAmarelo.addEventListener("click", function ligarFarol() {
+    farolAmarelo.classList.toggle("cor-amarela")
+    farolVermelho.classList.remove("cor-vermelha")
+    farolVerde.classList.remove("cor-verde")
+
+})
+btnVerde.addEventListener("click", function ligarFarol() {
+    farolVerde.classList.toggle("cor-verde")
+    farolAmarelo.classList.remove("cor-amarela")
+    farolVermelho.classList.remove("cor-vermelha")
+})
