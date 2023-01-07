4069edf8a358e9db0522e827d3d020ed6a39bb73
Gustavo Lopes <gustavoclash3@gmail.com>
2023-01-06 21:04:21 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var farolVermelho = document.getElementById("farolVermelho")
+var farolAmarelo = document.getElementById("farolAmarelo")
+var farolVerde = document.getElementById("farolVerde")
+
+var btnVermelho = document.getElementById("btnVermelho")
+var btnAmarelo = document.getElementById("btnAmarelo")
+var btnVerde = document.getElementById("btnVerde")
+
+btnVermelho.addEventListener("click", function ligarFarol() {
+    farolVermelho.classList.add("cor-vermelha")
+}) 
