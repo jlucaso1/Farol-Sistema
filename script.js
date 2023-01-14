9f93b6184d368d6624700d1f4f4a407d72bc2b84
João Lucas de Oliveira Lopes <55464917+jlucaso1@users.noreply.github.com>
2023-01-14 19:06:46 +0000

    chore: iniciar sistema de semaforo
    Co-authored-by: Gustavo Lopes <glopes7@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,25 +1,124 @@
-var farolVermelho = document.getElementById("farolVermelho")
-var farolAmarelo = document.getElementById("farolAmarelo")
-var farolVerde = document.getElementById("farolVerde")
+class Semaforo {
+  // propriedades (variáveis de estado)
+  luzAmarela = false;
+  luzVerde = false;
+  luzVermelha = false;
+  farolVermelho;
+  farolAmarelo;
+  farolVerde;
 
-var btnVermelho = document.getElementById("btnVermelho")
-var btnAmarelo = document.getElementById("btnAmarelo")
-var btnVerde = document.getElementById("btnVerde")
+  constructor(idAmarela, idVermelha, idVerde) {
+    this.farolVermelho = document.getElementById(idVermelha);
+    this.farolAmarelo = document.getElementById(idAmarela);
+    this.farolVerde = document.getElementById(idVerde);
+  }
 
-btnVermelho.addEventListener("click", function ligarFarol() {
-    farolVermelho.classList.toggle("cor-vermelha")
-    farolAmarelo.classList.remove("cor-amarela")
-    farolVerde.classList.remove("cor-verde")
-})
+  ligarLuz(luz) {
+    // 1 - Verifica se já está ativa/ligada
+    // se estiver ele apenas desliga
+    switch (luz) {
+      case "verde":
+        if (this.luzVerde) {
+          this.desligarLuz(luz);
+          return;
+        }
 
-btnAmarelo.addEventListener("click", function ligarFarol() {
-    farolAmarelo.classList.toggle("cor-amarela")
-    farolVermelho.classList.remove("cor-vermelha")
-    farolVerde.classList.remove("cor-verde")
+        break;
+      case "vermelha":
+        if (this.luzVermelha) {
+          this.desligarLuz(luz);
+          return;
+        }
+        break;
+      case "amarela":
+        if (this.luzAmarela) {
+          this.desligarLuz(luz);
+          return;
+        }
+        break;
+      default:
+        console.error("Valor não existe");
+        break;
+    }
 
-})
-btnVerde.addEventListener("click", function ligarFarol() {
-    farolVerde.classList.toggle("cor-verde")
-    farolAmarelo.classList.remove("cor-amarela")
-    farolVermelho.classList.remove("cor-vermelha")
-})
+    this.desligarLuzes();
+
+    // Ligar uma luz apenas
+    switch (luz) {
+      case "verde":
+        this.luzVerde = true;
+        this.farolVerde.classList.add("cor-verde");
+        break;
+      case "vermelha":
+        this.luzVermelha = true;
+        this.farolVermelho.classList.add("cor-vermelha");
+        break;
+      case "amarela":
+        this.luzAmarela = true;
+        this.farolAmarelo.classList.add("cor-amarela");
+        break;
+      default:
+        console.error("Valor não existe");
+        break;
+    }
+  }
+
+  desligarLuz(luz) {
+    switch (luz) {
+      case "verde":
+        this.farolVerde.classList.remove("cor-verde");
+        this.luzVerde = false;
+
+        break;
+      case "vermelha":
+        this.farolVermelho.classList.remove("cor-vermelha");
+        this.luzVermelha = false;
+        break;
+      case "amarela":
+        this.farolAmarelo.classList.remove("cor-amarela");
+        this.luzAmarela = false;
+        break;
+      default:
+        console.error("Valor não existe");
+        break;
+    }
+  }
+
+  desligarLuzes() {
+    this.luzVerde = false;
+    this.luzVermelha = false;
+    this.luzAmarela = false;
+    this.farolAmarelo.classList.remove("cor-amarela");
+    this.farolVermelho.classList.remove("cor-vermelha");
+    this.farolVerde.classList.remove("cor-verde");
+  }
+
+  iniciarSemaforo() {
+    this.ligarLuz("verde");
+    setTimeout(() => {
+      this.ligarLuz("amarela");
+
+      setTimeout(() => {
+        this.ligarLuz("vermelha");
+        setTimeout(() => {
+          this.iniciarSemaforo();
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }
+}
+
+// Exemplo teste
+
+const semaforo1 = new Semaforo("farolAmarelo", "farolVermelho", "farolVerde");
+const semaforo2 = new Semaforo(
+  "farolAmarelo2",
+  "farolVermelho2",
+  "farolVerde2"
+);
+
+semaforo1.iniciarSemaforo();
+
+setTimeout(() => {
+  semaforo2.iniciarSemaforo();
+}, 2000);
